918a7871feb19dc05b0ebca7e490b59792e97d0e
oniiru <oneal.andrew@gmail.com>
2013-03-28 15:24:49 -0700

    more mixpanel stuff

--- html/wp-content/themes/bootstrap-child-theme/footercheckout.php
+++ html/wp-content/themes/bootstrap-child-theme/footercheckout.php
@@ -26,7 +26,15 @@
 		<![endif]-->
 		
 		<?php wp_footer(); // js scripts are inserted using this function ?>
-
+		<!-- MixPanel Start -->
+		<script type="text/javascript">
+		mixpanel.identify('<?php global $current_user;
+		get_currentuserinfo(); echo $current_user->ID; ?>');
+		mixpanel.track('Viewed Post', {
+		    "Title":"Visited <?php single_post_title(); ?>",
+		});
+		</script>
+		<!-- MixPanel End -->
 	</body>
 
 </html>
