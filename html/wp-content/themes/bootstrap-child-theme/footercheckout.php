d07650dee6900bdbdc4e3b053e47fcabda2254a5
oniiru <oneal.andrew@gmail.com>
2013-03-30 11:53:23 -0700

    asdf

--- html/wp-content/themes/bootstrap-child-theme/footercheckout.php
+++ html/wp-content/themes/bootstrap-child-theme/footercheckout.php
@@ -12,9 +12,27 @@
 		<?php wp_footer(); // js scripts are inserted using this function ?>
 		<!-- MixPanel Start -->
 		<script type="text/javascript">
+		<?php if (is_user_logged_in()) { ?>
 		mixpanel.identify('<?php global $current_user;
 		get_currentuserinfo(); echo $current_user->ID; ?>');
+		<?php } ?>
+		
 		mixpanel.track('Viewed <?php single_post_title(); ?>');
+		mixpanel.track_links('.accordion-toggle', 'Opened FAQ', {
+		    'Page': '<?php the_title(); ?>'
+		});
+		mixpanel.track_links('.businessplan a', 'Clicked Business Plan Contact Us');
+		   
+		mixpanel.track_links('.freeplan a', 'Clicked Signup Button', {
+		    'Type': 'Free Plan',
+		});
+		mixpanel.track_links('.monthlyplan a', 'Clicked Signup Button', {
+		    'Type': 'Standard Plan',
+		    'Page': '<?php the_title(); ?>'
+		});
+		mixpanel.track_links('#other_discount_code_a', 'Clicked Coupon Link');
+		
+		
 		</script>
 		<!-- MixPanel End -->
 		
