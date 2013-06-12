590e9bd43ad8dc8a9536c85afd5180a683609dad
Andrew ONeal <oneal.andrew@gmail.com>
2013-06-12 13:43:29 -0700

    webinar change

--- html/wp-content/themes/bootstrap-child-theme/webinars.php
+++ html/wp-content/themes/bootstrap-child-theme/webinars.php
@@ -21,27 +21,27 @@ Template Name: Webinars
 				<h1 class="webinars">Get your questions answered with SolidWize Webinars</h1>
 				<div id="webinars">
 					<div id="webinarsleft">
-<iframe src="http://player.vimeo.com/video/57401110?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1" width="620" height="368" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+<iframe src="http://player.vimeo.com/video/68000787?title=0&amp;byline=0&amp;portrait=0" width="620" height="388" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
 		</div>
 				<div id="webinarsright">
-				<h3 style="font-weight:700;font-size: 22px !important;margin: 20px 0 0px">Equation Driven Design</h3>
+				<h3 style="font-weight:700;font-size: 21px !important;margin: 20px 0 0px">Beginning Simulation</h3>
 				<h4 style="margin-top: 0px;
 font-size:18px !important"><b>Free </b>SolidWorks Webinar</h4><br>
-<div style="border-bottom:1px dotted black;padding-bottom:20px;">Learn to use equations and global variables to design more efficiently.</div>
+<div style="border-bottom:1px dotted black;padding-bottom:20px;">Learn the basics of static stress analysis and weight reduction using SolidWorks Simulation.</div>
 <!-- 				<h3 style="font-size:1em;margin-top:10px;margin-bottom:20px;">We're holding two sessions: </h3> -->				
 		<p style="font-size: 24px !important;
 color: #000;
 font-weight: bold;
 line-height: 1.4em;
-margin-top: 15px; ">Thurs. January 17th <br> 10am PST (1pm EST)</p>
-				<a href="https://attendee.gotowebinar.com/rt/6253894943144082688" style="margin-top: 20px;
+margin-top: 15px; ">Thurs. June 20th <br> 10am PST (1pm EST)</p>
+				<a href="https://attendee.gotowebinar.com/register/7723988172259659008" style="margin-top: 20px;
 " class="btn btn-large btn-success" target="_blank">Sign Up Now!</a>
 
 <!-- 				<div style="font-size:11px; line-height:1em;"> (Be sure to select the session you wish to join on the next page.) </div> -->				</div>
 <div class="fb-like" style="margin-top:20px" data-href="http://facebook.com/solidwize" data-send="true" data-width="450" data-show-faces="false"></div>
 		</div>
 				<div id="webinarstext">
-					This week, we'll be talking about using equations and global variables in your models in order to automate your designs, make it easy to build configurations, and link dimensions between models. Virtually any SolidWorks user can benefit from incorporating these techniques into their designs, so if this sounds like something you are interested in sign up for our Free webinar on equation driven design.
+					This week, we'll be looking at SolidWorks Simulation, and introducing the concepts of solid stress analysis by examining a brake pedal. We'll also be using design insight from the simulation to reduce the mass of the part. As always, this 45 minute webinar will be followed by a Q&A period so come with those burning questions you've been dying to ask.
 </div>
 
 		</div><!-- end content -->
