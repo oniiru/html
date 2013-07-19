55ad277c41df4fb45a8fbc237a96ecb4ac5ad1a5
Andrew ONeal <oneal.andrew@gmail.com>
2013-07-19 16:34:47 -0700

    fix

--- html/wp-content/plugins/Project_View/project_template.php
+++ html/wp-content/plugins/Project_View/project_template.php
@@ -173,9 +173,9 @@ if ($appendix1 != '') { ?>
 		 ?>
 		 <div class="accordion-group">
 		     <div class="accordion-heading">
-		       <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+		       <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapse-<?php the_ID(); ?>">
 		 <h4><?php echo $techniquemeta['lessontitle'] ?></h4>
-		 <div id="collapseOne" class="accordion-body collapse in">
+		 <div id="collapse-<?php the_ID(); ?>" class="accordion-body collapse in">
 		      <div class="accordion-inner">
 		 <?php
 		 
