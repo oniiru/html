211990ede69b0c171e7e83da8300f1f9b8250f80
Andrew ONeal <oneal.andrew@gmail.com>
2013-10-27 01:02:40 -0700

    fav

--- wp-content/themes/bootstrap-child-theme/header.php
+++ wp-content/themes/bootstrap-child-theme/header.php
@@ -21,13 +21,13 @@
 		
 		<!-- icons & favicons -->
 		<!-- For iPhone 4 -->
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/h/apple-touch-icon.png">
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/images/solidfav118.jpg">
 		<!-- For iPad 1-->
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/m/apple-touch-icon.png">
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/images/solidfav72.jpg">
 		<!-- For iPhone 3G, iPod Touch and Android -->
-		<link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/l/apple-touch-icon-precomposed.png">
+		<link rel="apple-touch-icon-precomposed" href="<?php echo get_stylesheet_directory_uri(); ?>/images/solidfav.jpg">
 		<!-- For Nokia -->
-		<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/l/apple-touch-icon.png">
+		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/images/solidfav.jpg">
 		<!-- For everything else -->
 		<link rel="shortcut icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/images/solidfav.jpg" />
 		<!-- media-queries.js (fallback) -->
