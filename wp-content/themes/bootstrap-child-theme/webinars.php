67ad8342764761d9a8985a720a528bff70a243e6
Andrew ONeal <oneal.andrew@gmail.com>
2013-10-30 15:03:46 -0700

    webinarsss

--- wp-content/themes/bootstrap-child-theme/webinars.php
+++ wp-content/themes/bootstrap-child-theme/webinars.php
@@ -21,27 +21,28 @@ Template Name: Webinars
 				<h1 class="webinars">Get your questions answered with SolidWize Webinars</h1>
 				<div id="webinars">
 					<div id="webinarsleft">
-<iframe src="http://player.vimeo.com/video/68000787?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1" width="620" height="388" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+<iframe src="//player.vimeo.com/video/77524058?title=0&amp;byline=0&amp;portrait=0" width="620" height="388" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 		</div>
 				<div id="webinarsright">
-				<h3 style="font-weight:700;font-size: 21px !important;margin: 20px 0 0px">Beginning Simulation</h3>
+				<h3 style="font-weight:700;font-size: 21px !important;margin: 20px 0 0px">Faster Modeling Part 1</h3>
 				<h4 style="margin-top: 0px;
 font-size:18px !important"><b>Free </b>SolidWorks Webinar</h4><br>
-<div style="border-bottom:1px dotted black;padding-bottom:20px;">Learn the basics of static stress analysis and weight reduction using SolidWorks Simulation.</div>
+<div style="border-bottom:1px dotted black;padding-bottom:20px;">Learn the tips and tricks to speed up your modeling process. We'll cover shortcuts, sketchtools, design library, surfacing tools, and general tips to speed up your part modeling.
+</div>
 <!-- 				<h3 style="font-size:1em;margin-top:10px;margin-bottom:20px;">We're holding two sessions: </h3> -->				
 		<p style="font-size: 24px !important;
 color: #000;
 font-weight: bold;
 line-height: 1.4em;
-margin-top: 15px; ">Thurs. June 20th <br> 10am PST (1pm EST)</p>
-				<a href="https://attendee.gotowebinar.com/register/7723988172259659008" style="margin-top: 20px;
+margin-top: 15px; ">Tuesday Nov. 5th <br> 10am PST (1pm EST)</p>
+				<a href="https://attendee.gotowebinar.com/register/7138146389845889026" style="margin-top: 5px;
 " class="btn btn-large btn-success" target="_blank">Sign Up Now!</a>
 
 <!-- 				<div style="font-size:11px; line-height:1em;"> (Be sure to select the session you wish to join on the next page.) </div> -->				</div>
 <div class="fb-like" style="margin-top:20px" data-href="http://facebook.com/solidwize" data-send="true" data-width="450" data-show-faces="false"></div>
 		</div>
 				<div id="webinarstext">
-					This week, we'll be looking at SolidWorks Simulation, and introducing the concepts of solid stress analysis by examining a brake pedal. We'll also be using design insight from the simulation to reduce the mass of the part. As always, this 45 minute webinar will be followed by a Q&A period so come with those burning questions you've been dying to ask.
+					We have a great webinar on faster modeling in SolidWorks. This webinar is going to be part of a series in which I cover all kinds of tips and tricks to producing quicker solidworks designs. For part one, I'll be taking you through quite a few SolidWorks shortcuts and more efficient sketching and part modeling. If that sounds like something of interest to you, sign up for our free webinar on faster modeling in solidworks.
 </div>
 
 		</div><!-- end content -->
